80b99bc483ab8512f8ab9c4e28a6d4448d17ebcb
Sonu Shivcharan <sonushivcharan@outlook.com>
2024-03-13 19:49:47 +0530

    update dist/App.js

--- dist/App.js
+++ dist/App.js
@@ -1,5 +1,6 @@
 import AboutMeSection from "./components/AboutMe.js";
 import SharePanel from "./components/SharePanel.js"
+import Footer from "./components/Footer.js"
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
@@ -616,60 +617,6 @@ class ContactSection extends React.Component {
             React.createElement(ContactForm, null)))));
   }}
 
-class Footer extends React.Component {
-  render() {
-    const {profiles} = this.props;
-
-    return /*#__PURE__*/(
-      React.createElement("footer", {
-        id: "footer"
-      }, /*#__PURE__*/
-        React.createElement("div", {
-          className: "footer-links"
-        },
-          profiles &&
-          profiles.map(profile => {
-            return /*#__PURE__*/(
-              React.createElement("a", {
-                href: profile.url, key: profile.name
-              }, /*#__PURE__*/
-                React.createElement("i", {
-                  className: `${profile.fontAwesomeClass} footer-icon`
-                })));
-          })), /*#__PURE__*/
-
-        React.createElement("p", null,
-          " ", "Built with ", /*#__PURE__*/
-          React.createElement("i", {
-            className: "fa fa-heart",
-            style: {
-              color: "#ff4889",
-              fontSize:"1.5rem",
-              transform: "translateY(4px)"
-            }
-          }),
-          " ", "using",
-          " ", /*#__PURE__*/
-          React.createElement("i", {
-            className: "fa-brands fa-react",
-            style: {
-              fontSize: "1.5rem",
-              color: "#47cbfe",
-              transform: "translateY(4px)"
-            }
-          }),
-
-          " "), /*#__PURE__*/
-        React.createElement("br", null), /*#__PURE__*/
-        React.createElement("div", {
-          className: "copy"
-        }, /*#__PURE__*/
-          React.createElement("p", null, /*#__PURE__*/
-            React.createElement("i", {
-              className: "fa fa-copyright"
-            }), " Copyright Sonu Shivcharan"))));
-
-  }}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
