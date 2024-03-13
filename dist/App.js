2bbaf18121346e0dc5d0060bcacd284e943eaee9
Sonu Shivcharan <sonushivcharan@outlook.com>
2024-03-13 19:46:06 +0530

    update dist/App.js

--- dist/App.js
+++ dist/App.js
@@ -1,4 +1,5 @@
 import AboutMeSection from "./components/AboutMe.js";
+import SharePanel from "./components/SharePanel.js"
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-analytics.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-database.js";
@@ -171,66 +172,6 @@ class App extends React.Component {
 
   }}
 
-class SharePanel extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return /*#__PURE__*/(
-      React.createElement("div", {
-        className: "panel-container flex",
-        style: {
-          width: "100%", position: "relative"
-        }
-      }, /*#__PURE__*/
-
-        React.createElement("div", {
-          id: "share-panel", style: {
-            animation: "0.1s slideUp ease 1"
-          }
-        }, /*#__PURE__*/
-          React.createElement("h4", null, "Share Via"), /*#__PURE__*/
-          React.createElement("div", {
-            className: "flex"
-          }, /*#__PURE__*/
-            React.createElement("a", {
-              href: "whatsapp://send?text=https://sonu-shivcharan.github.io",
-              "data-action": "share/whatsapp/share",
-              target: "_blank",
-              className: "share-icon"
-            }, /*#__PURE__*/
-
-              React.createElement("i", {
-                className: "fa-brands fa-whatsapp"
-              })), /*#__PURE__*/
-
-            React.createElement("a", {
-              href: "#", className: "share-icon"
-            }, /*#__PURE__*/
-              React.createElement("i", {
-                className: "fa-brands fa-facebook"
-              })), /*#__PURE__*/
-
-            React.createElement("a", {
-              href: "#", className: "share-icon"
-            }, /*#__PURE__*/
-              React.createElement("i", {
-                className: "fa fa-envelope"
-              })), /*#__PURE__*/
-
-            React.createElement("a", {
-              href: "#", className: "share-icon"
-            }, /*#__PURE__*/
-              React.createElement("i", {
-                className: "fa fa-copy"
-              }))))));
-
-
-
-
-
-  }}
 
 class Header extends React.Component {
   constructor(props) {
